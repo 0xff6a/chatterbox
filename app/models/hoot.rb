2349d0dbccfb78beba09d824692d11b75f61b4f5
Jeremy Fox <foxjerem@gmail.com>
2014-08-01 12:12:56 +0100

    Added user-hoot assocation

--- app/models/hoot.rb
+++ app/models/hoot.rb
@@ -2,6 +2,8 @@ class Hoot
 
 	include DataMapper::Resource
 
+	belongs_to :user
+
 	property 	:id, 				Serial
 	property 	:content, 	Text, :required => true, 
 															:message => 'Your hoot must have content'
--- spec/hoot_spec.rb
+++ spec/hoot_spec.rb
@@ -1,19 +1,28 @@
 describe Hoot do
 	
+	before(:each) { create_test_user }
+
 	it 'should be created with content' do
-		Hoot.create(:content => 'Hello')
+		Hoot.create(:content => 'Hello', :user_id => user.id)
 		expect(Hoot.count).to eq(1)
 		expect(hoot.content).to eq('Hello')
 	end
 
 	it 'cannot be created without content' do
-		Hoot.create()
+		Hoot.create(:user_id => user.id)
 		expect(Hoot.count).to eq(0)
 	end
 
 	it 'should have a timestamp' do
-		Hoot.create(:content => 'Hello', :timestamp => Time.now)
+		Hoot.create(:content => 'Hello', :timestamp => Time.now, :user_id => user.id)
 		expect(Time.parse(hoot.timestamp)).to be_within(60).of(Time.now)
+	end
+
+	it 'should belong to a user' do
+		Hoot.create(:content => 'Hello')
+		expect(Hoot.count).to eq(0)
+		Hoot.create(:content => 'Hello', :user_id => user.id)
+		expect(Hoot.count).to eq(1)
 	end
 
 	def hoot
