d3af55eefd916596d3447ee80de93e552cd47b7d
Jeremy Fox <foxjerem@gmail.com>
2014-08-03 16:18:17 +0100

    Bug fix

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -2,7 +2,7 @@ class Mailer
 
 		def self.send_message(to, subject, message_body)
 			 RestClient.post "https://api:#{ENV['MAILGUN_API_KEY']}"\
-		  "@api.mailgun.net/v2/app2809752.mailgun.org/messages",
+		  "@api.mailgun.net/v2/app28097521.mailgun.org/messages",
 		  :from => "ChatterBOX <#{ENV['MAILGUN_SMTP_LOGIN']}>",
 		  :to => to,
 		  :subject => subject,
